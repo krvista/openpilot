515761488e93fc01bb0e4e147e6918bed3582fdf
Kay Oh <krvista@gmail.com>
2026-03-31 12:02:04 +0900

    Modify Hyundai Ioniq 6 N specs and documentation
    Updated tire stiffness factor for Hyundai Ioniq 6 N.

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -399,10 +399,10 @@ class CAR(Platforms):
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
   )
   HYUNDAI_IONIQ_6_N = HyundaiCanFDPlatformConfig(
-    [HyundaiCarDocs("Hyundai Ioniq 6 N (with HDA II) 2026", car_parts=CarParts.common([CarHarness.hyundai_s]))],
-    CarSpecs(mass=2175, wheelbase=2.965, steerRatio=14.26, tireStiffnessFactor=1.0),
+    [HyundaiCarDocs("Hyundai Ioniq 6 N (with HDA II) 2026", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_s]))],
+    CarSpecs(mass=2175, wheelbase=2.965, steerRatio=14.26, tireStiffnessFactor=1.1),
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
-  )  
+  ) 
   HYUNDAI_TUCSON_4TH_GEN = HyundaiCanFDPlatformConfig(
     [
       HyundaiCarDocs("Hyundai Tucson 2022", car_parts=CarParts.common([CarHarness.hyundai_n])),
